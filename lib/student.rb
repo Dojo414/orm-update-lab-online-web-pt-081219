e93cbebe78e22ce6ea4d338c1ba84554b14f17dc
Donovan Jones <donovanljones94@gmail.com>
2019-10-30 22:27:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,18 @@ attr_accessor :name, :id, :grade
     @name = name
     @grade = grade
   end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT)
+      SQL
+      
+      DB[:conn]
+  end
+     
 
 
 end

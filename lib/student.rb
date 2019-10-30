b0824b590a3a39870b29d268610452f70b7b1d46
Donovan Jones <donovanljones94@gmail.com>
2019-10-30 22:34:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,7 +41,10 @@ attr_accessor :name, :id, :grade
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create 
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+  end
      
 
 
